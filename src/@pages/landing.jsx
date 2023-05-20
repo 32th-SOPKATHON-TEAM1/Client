37a92cc02ba98a52bb0fc74caee64a8087844902
Seojisoo20191941 <amy0480@naver.com>
2023-05-21 01:21:45 +0900

    [ design ] 버튼 디자인 표기만 구현

--- src/@pages/landing.jsx
+++ src/@pages/landing.jsx
@@ -1,5 +1,6 @@
 import React from "react";
+import Button from "../@components/common/button";
 
 export default function Landing() {
-  return <div>landing</div>;
+  return <Button text="이전" width={5} />;
 }

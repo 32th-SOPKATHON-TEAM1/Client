7c93f0974228dc9c28097d5ecf0ee8583701664a
Seojisoo20191941 <amy0480@naver.com>
2023-05-21 07:58:10 +0900

    fix

--- src/api/getPhotoInfo.jsx
+++ src/api/getPhotoInfo.jsx
@@ -1,22 +1,22 @@
-// import axios from "axios";
-
-// export default async function getPhotoInfo(id) {
-//   try {
-//     const data = await axios.get(`https://api.mindphoto.kro.kr/post/${id}`);
-//     return data.data;
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
-
 import axios from "axios";
 
-export async function getPhotoInfo(id) {
+export default async function getPhotoInfo(id) {
   try {
     const data = await axios.get(`https://api.mindphoto.kro.kr/post/${id}`);
-    console.log(data);
     return data.data;
   } catch (error) {
     console.log(error);
   }
 }
+
+// import axios from "axios";
+
+// export async function getPhotoInfo(id) {
+//   try {
+//     const data = await axios.get(`https://api.mindphoto.kro.kr/post/${id}`);
+//     console.log(data);
+//     return data.data;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }

0cd3ae6735b2ae20a0e8741a67db6758408903b8
Seojisoo20191941 <amy0480@naver.com>
2023-05-21 04:33:38 +0900

    [ feat ] 백그라운드 이미지 삽입

--- src/@pages/emotion.jsx
+++ src/@pages/emotion.jsx
@@ -1,13 +1,28 @@
 import React, { useState } from "react";
-import StepRender from "../@components/emotion/StepRenderer"
+import StepRender from "../@components/emotion/StepRenderer";
 import ProgressBar from "../@components/common/progressBar";
+import BackgroundImg from "../assets/image/backgroundImg.png";
+import { styled } from "styled-components";
 
 export default function Emotion() {
   const [step, setStep] = useState(1);
   return (
     <>
-    <ProgressBar step={step} />
-    <StepRender step={step} setStep={setStep} />
+      <ProgressBar step={step} />
+      <StepRender step={step} setStep={setStep} />
+      <Background />
     </>
   );
 }
+
+const Background = styled.div`
+  width: 360px;
+  height: 800px;
+
+  position: absolute;
+  z-index: -1;
+  top: 0;
+  margin-left: -1.4rem;
+
+  background-image: url(${BackgroundImg});
+`;
--- src/@pages/landing.jsx
+++ src/@pages/landing.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import ProgressBar from "../@components/common/progressBar";
 
 export default function Landing() {
-  return <ProgressBar step={1} />;
+  return <div>landing</div>;
 }

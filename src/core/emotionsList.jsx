4d9f1af708f4ab3179856f9cd775edd33604130c
Seojisoo20191941 <amy0480@naver.com>
2023-05-21 03:57:55 +0900

    [ feat ] 상수 파일 구현

--- src/core/emotionsList.jsx
+++ src/core/emotionsList.jsx
@@ -1,1 +1,57 @@
-export const EMOTIONS = {};
+export const EMOTIONS = [
+  { id: 0, emotion: "기쁨" },
+  { id: 1, emotion: "두려움" },
+  { id: 2, emotion: "불쾌" },
+  { id: 3, emotion: "슬픔" },
+];
+
+export const HAPPY = [
+  { id: 0, emotion: "감사" },
+  { id: 1, emotion: "만족" },
+  { id: 2, emotion: "설렘" },
+  { id: 3, emotion: "신남" },
+  { id: 4, emotion: "자랑스러움" },
+  { id: 5, emotion: "편안" },
+  { id: 6, emotion: "행복" },
+  { id: 7, emotion: "홀가분" },
+];
+
+export const SCARED = [
+  { id: 0, emotion: "긴장" },
+  { id: 1, emotion: "당황" },
+  { id: 2, emotion: "두려움" },
+  { id: 3, emotion: "무서움" },
+  { id: 4, emotion: "불안" },
+  { id: 5, emotion: "혼란" },
+  { id: 6, emotion: "걱정" },
+  { id: 7, emotion: "놀라움" },
+];
+
+export const UNPLEASANT = [
+  { id: 0, emotion: "답답함" },
+  { id: 1, emotion: "억울함" },
+  { id: 2, emotion: "원망" },
+  { id: 3, emotion: "지루함" },
+  { id: 4, emotion: "부끄러움" },
+  { id: 5, emotion: "어색함" },
+  { id: 6, emotion: "피곤함" },
+  { id: 7, emotion: "부러움" },
+];
+
+export const SORROWFUL = [
+  { id: 0, emotion: "괴로움" },
+  { id: 1, emotion: "그리움" },
+  { id: 2, emotion: "미안함" },
+  { id: 3, emotion: "실망" },
+  { id: 4, emotion: "서운함" },
+  { id: 5, emotion: "외로움" },
+  { id: 6, emotion: "우울함" },
+  { id: 7, emotion: "후회" },
+];
+
+export const Today = [
+  { id: 0, emotion: "평생 간직하고 싶어요" },
+  { id: 1, emotion: "친구들과 나누고 싶어요" },
+  { id: 2, emotion: "오늘까지만 기억할래요" },
+  { id: 3, emotion: "없던 일이었으면 좋겠어요" },
+];

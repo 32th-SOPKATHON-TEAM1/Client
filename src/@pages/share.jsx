1bb9042a867f76559ee3c0eb1e32618d47ea2fb9
KwonET <dlxl3652zz@gmail.com>
2023-05-21 01:53:46 +0900

    [ feat ] share에 쓰이는 getPhotoAPI, fetchPhotoAPI 함수 구현

--- src/@pages/share.jsx
+++ src/@pages/share.jsx
@@ -1,5 +1,19 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import getPhotoInfo from "../api/getPhotoInfo";
 
 export default function Share() {
+  const [photos, setPhotos] = useState();
+  async function fetchPhotoInfo() {
+    const response = await getPhotoInfo();
+    setPhotos(response);
+  }
+  
+  useEffect(() => {
+    fetchPhotoInfo;
+  }, []);
+  console.log(photos);
+
   return <div>share</div>;
 }

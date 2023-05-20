06b422089fe57b1de276a30eedb206723ca7d619
Seojisoo20191941 <amy0480@naver.com>
2023-05-21 04:26:08 +0900

    [ design ] 프로그레스바 스타일링

--- src/@pages/share.jsx
+++ src/@pages/share.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import { useEffect } from "react";
-
 import { useNavigate, useLocation } from "react-router-dom";
 import { useState, useEffect } from "react";
 import styled from "styled-components";

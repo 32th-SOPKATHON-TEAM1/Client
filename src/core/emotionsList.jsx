01c171416d3f1128caba53e50149b94297888bcb
Seojisoo20191941 <amy0480@naver.com>
2023-05-21 03:59:39 +0900

    [ feat ] 상수 파일 칼럼 수정

--- src/core/emotionsList.jsx
+++ src/core/emotionsList.jsx
@@ -1,4 +1,4 @@
-export const EMOTIONS = [
+export const detailEmotionS = [
   { id: 0, emotion: "기쁨" },
   { id: 1, emotion: "두려움" },
   { id: 2, emotion: "불쾌" },
@@ -6,52 +6,52 @@ export const EMOTIONS = [
 ];
 
 export const HAPPY = [
-  { id: 0, emotion: "감사" },
-  { id: 1, emotion: "만족" },
-  { id: 2, emotion: "설렘" },
-  { id: 3, emotion: "신남" },
-  { id: 4, emotion: "자랑스러움" },
-  { id: 5, emotion: "편안" },
-  { id: 6, emotion: "행복" },
-  { id: 7, emotion: "홀가분" },
+  { id: 0, detailEmotion: "감사" },
+  { id: 1, detailEmotion: "만족" },
+  { id: 2, detailEmotion: "설렘" },
+  { id: 3, detailEmotion: "신남" },
+  { id: 4, detailEmotion: "자랑스러움" },
+  { id: 5, detailEmotion: "편안" },
+  { id: 6, detailEmotion: "행복" },
+  { id: 7, detailEmotion: "홀가분" },
 ];
 
 export const SCARED = [
-  { id: 0, emotion: "긴장" },
-  { id: 1, emotion: "당황" },
-  { id: 2, emotion: "두려움" },
-  { id: 3, emotion: "무서움" },
-  { id: 4, emotion: "불안" },
-  { id: 5, emotion: "혼란" },
-  { id: 6, emotion: "걱정" },
-  { id: 7, emotion: "놀라움" },
+  { id: 0, detailEmotion: "긴장" },
+  { id: 1, detailEmotion: "당황" },
+  { id: 2, detailEmotion: "두려움" },
+  { id: 3, detailEmotion: "무서움" },
+  { id: 4, detailEmotion: "불안" },
+  { id: 5, detailEmotion: "혼란" },
+  { id: 6, detailEmotion: "걱정" },
+  { id: 7, detailEmotion: "놀라움" },
 ];
 
 export const UNPLEASANT = [
-  { id: 0, emotion: "답답함" },
-  { id: 1, emotion: "억울함" },
-  { id: 2, emotion: "원망" },
-  { id: 3, emotion: "지루함" },
-  { id: 4, emotion: "부끄러움" },
-  { id: 5, emotion: "어색함" },
-  { id: 6, emotion: "피곤함" },
-  { id: 7, emotion: "부러움" },
+  { id: 0, detailEmotion: "답답함" },
+  { id: 1, detailEmotion: "억울함" },
+  { id: 2, detailEmotion: "원망" },
+  { id: 3, detailEmotion: "지루함" },
+  { id: 4, detailEmotion: "부끄러움" },
+  { id: 5, detailEmotion: "어색함" },
+  { id: 6, detailEmotion: "피곤함" },
+  { id: 7, detailEmotion: "부러움" },
 ];
 
 export const SORROWFUL = [
-  { id: 0, emotion: "괴로움" },
-  { id: 1, emotion: "그리움" },
-  { id: 2, emotion: "미안함" },
-  { id: 3, emotion: "실망" },
-  { id: 4, emotion: "서운함" },
-  { id: 5, emotion: "외로움" },
-  { id: 6, emotion: "우울함" },
-  { id: 7, emotion: "후회" },
+  { id: 0, detailEmotion: "괴로움" },
+  { id: 1, detailEmotion: "그리움" },
+  { id: 2, detailEmotion: "미안함" },
+  { id: 3, detailEmotion: "실망" },
+  { id: 4, detailEmotion: "서운함" },
+  { id: 5, detailEmotion: "외로움" },
+  { id: 6, detailEmotion: "우울함" },
+  { id: 7, detailEmotion: "후회" },
 ];
 
 export const Today = [
-  { id: 0, emotion: "평생 간직하고 싶어요" },
-  { id: 1, emotion: "친구들과 나누고 싶어요" },
-  { id: 2, emotion: "오늘까지만 기억할래요" },
-  { id: 3, emotion: "없던 일이었으면 좋겠어요" },
+  { id: 0, sentence: "평생 간직하고 싶어요" },
+  { id: 1, sentence: "친구들과 나누고 싶어요" },
+  { id: 2, sentence: "오늘까지만 기억할래요" },
+  { id: 3, sentence: "없던 일이었으면 좋겠어요" },
 ];

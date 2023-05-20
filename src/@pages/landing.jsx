857fc7612289702d77db43bd2d393c7d9854074d
Seojisoo20191941 <amy0480@naver.com>
2023-05-21 01:22:26 +0900

    [ revert ] 랜딩 페이지 테스트 지우기

--- src/@pages/landing.jsx
+++ src/@pages/landing.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import Button from "../@components/common/button";
 
 export default function Landing() {
-  return <Button text="이전" width={5} />;
+  return <div>landing</div>;
 }
